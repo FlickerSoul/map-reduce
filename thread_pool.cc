92bd9d6b56389bc04443d68bd62c8cf3cf61a602
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-11-07 19:05:32 -0800

    damn template

--- thread_pool.cc
+++ thread_pool.cc
@@ -1,49 +1,49 @@
 #include "thread_pool.hh"
 
-template <typename T, typename W>
-ThreadPool<T, W>::ThreadPool(int thread_num, W worker, std::queue<std::tuple<T>> work_queue) {
-    this->thread_num = thread_num;
-    this->worker = worker;
-    this->work_queue = work_queue;
-}
+// template <typename T, typename W>
+// ThreadPool<T, W>::ThreadPool(int thread_num, std::queue<T> work_queue, W worker) {
+//     this->thread_num = thread_num;
+//     this->worker = worker;
+//     this->work_queue = work_queue;
+// }
 
-template <typename T, typename W>
-void ThreadPool<T, W>::remove_worked_thread(std::tuple<T> work_input) {
-    this->work_count_lock.lock();
-    this->working_future_map.erase(work_input);
-    this->current_running_thread_num -= 1;
-    this->work_count_lock.unlock();
-    this->accepting_work.notify_one();
-}
+// template <typename T, typename W>
+// void ThreadPool<T, W>::remove_worked_thread(T work_input) {
+//     this->work_count_lock.lock();
+//     this->working_future_map.erase(work_input);
+//     this->current_running_thread_num -= 1;
+//     this->work_count_lock.unlock();
+//     this->accepting_work.notify_one();
+// }
 
 
-template <typename T, typename W>
-void ThreadPool<T, W>::run_jobs() {
-    // mapper thread 
-    while (!this->work_queue.empty()) {
-        std::tuple<T> job_input = this->work_queue.pop();
+// template <typename T, typename W>
+// void ThreadPool<T, W>::run_jobs() {
+//     // mapper thread 
+//     while (!this->work_queue.empty()) {
+//         T job_input = this->work_queue.pop();
 
-        this->work_count_lock.lock();
-        while (this->thread_num == this->current_running_thread_num) {
-            this->accepting_work.wait(this->work_count_lock);
-        }
+//         this->work_count_lock.lock();
+//         while (this->thread_num == this->current_running_thread_num) {
+//             this->accepting_work.wait(this->work_count_lock);
+//         }
 
-        // worker thread 
-        this->working_future_map.emplace(job_input, 
-            std::async(this->job_helper, 
-                       job_input)
-        );
+//         // worker thread 
+//         this->working_future_map.emplace(job_input, 
+//             std::async(this->job_helper, 
+//                        job_input)
+//         );
 
-        this->work_count_lock.unlock();
-    }
+//         this->work_count_lock.unlock();
+//     }
 
-    for (auto const &[key, value]: this->working_future_map) {
-        value.wait();
-    }
-}
+//     for (auto const &[key, value]: this->working_future_map) {
+//         value.wait();
+//     }
+// }
 
-template <typename T, typename W>
-void ThreadPool<T, W>::job_helper(std::tuple<T> job_input) {
-    std::apply(this->worker, job_input);
-    this->remove_worked_thread(job_input);
-}
+// template <typename T, typename W>
+// void ThreadPool<T, W>::job_helper(T job_input) {
+//     std::apply(this->worker, job_input);
+//     this->remove_worked_thread(job_input);
+// }
--- thread_pool.hh
+++ thread_pool.hh
@@ -1,5 +1,6 @@
 #include <mutex>
 #include <queue>
+#include <tuple>
 #include <stdbool.h>
 #include <map>
 #include <future>
@@ -8,14 +9,8 @@ template <typename T, typename W>
 class ThreadPool {
 
     public:
-        ThreadPool(int thread_num, W worker, std::queue<std::tuple<T>> work_queue);
-
-        void add_job(std::tuple<T> input);
-        void run_jobs();
-
-    private: 
-        std::queue<std::tuple<T>> work_queue;
-        std::map<std::tuple<T>, std::future<void>> working_future_map;
+        std::queue<T> work_queue;
+        std::map<T, std::future<void>> working_future_map;
 
         W worker;
         
@@ -27,8 +22,50 @@ class ThreadPool {
 
         int counter = 0;
         bool terminate = false;
+        
+        ThreadPool(int thread_num, std::queue<T> work_queue, W worker) {
+            this->thread_num = thread_num;
+            this->worker = worker;
+            this->work_queue = work_queue;
+        }
 
-        void remove_worked_thread(std::tuple<T> job_input);
-        void job_helper(std::tuple<T> job_input);
+        // void add_job(T input);
+
+        void run_jobs() {
+            // mapper thread 
+            while (!this->work_queue.empty()) {
+                T job_input = this->work_queue.pop();
+
+                this->work_count_lock.lock();
+                while (this->thread_num == this->current_running_thread_num) {
+                    this->accepting_work.wait(this->work_count_lock);
+                }
+
+                // worker thread 
+                this->working_future_map.emplace(job_input, 
+                    std::async(this->job_helper, 
+                            job_input)
+                );
+
+                this->work_count_lock.unlock();
+            }
+
+            for (auto const &[key, value]: this->working_future_map) {
+                value.wait();
+            }
+        }
+
+        void remove_worked_thread(T job_input) {
+            this->work_count_lock.lock();
+            this->working_future_map.erase(work_input);
+            this->current_running_thread_num -= 1;
+            this->work_count_lock.unlock();
+            this->accepting_work.notify_one();
+        };
+
+        void job_helper(T job_input) {
+            std::apply(this->worker, job_input);
+            this->remove_worked_thread(job_input);
+        }
         
 };
