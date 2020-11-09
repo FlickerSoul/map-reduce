2438e2d98f1452ab9b1b9d0ee4a54311ba867760
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-11-08 23:18:56 -0800

    Update utilities_test.cc

--- utilities_test.cc
+++ utilities_test.cc
@@ -5,22 +5,28 @@ using namespace MR_Utilities;
 void init_global_storage() {
     GlobalStorage* storage = new GlobalStorage();
     assert(storage->storage_vector.size() == 0);
+    assert(storage->get_mapping_no_sync(1) == nullptr);
 
-    MapWrapper* m1 = storage->get_mapping(2);
+    MapWrapper* m1 = storage->get_mapping(0);
     assert (m1 != nullptr);
-    assert (m1 = storage->storage_vector[2]);
-    assert (storage->storage_vector.size() == 2);
+    assert (m1 = storage->storage_vector[0]);
+    assert (storage->storage_vector.size() == 1);
 
-    MapWrapper* m2 = storage->get_mapping(5);
+    MapWrapper* m2 = storage->get_mapping(2);
     assert (m2 != nullptr);
-    assert (m2 = storage->storage_vector[5]);
-    assert (storage->storage_vector.size() == 5);
+    assert (m2 = storage->storage_vector[2]);
+    assert (storage->storage_vector.size() == 3);
 
+    MapWrapper* m3 = storage->get_mapping(5);
+    assert (m3 != nullptr);
+    assert (m3 = storage->storage_vector[5]);
+    assert (storage->storage_vector.size() == 6);
 
-    MapWrapper* m3 = storage->get_mapping(1);
-    assert(m3 != nullptr);
-    assert(storage->storage_vector.size() == 5);
+    MapWrapper* m4 = storage->get_mapping(1);
+    assert(m4 != nullptr);
+    assert(storage->storage_vector.size() == 6);
 
+    assert(m1->get_list_no_sync("abc") == nullptr);
     assert(m3->mapping["abc"] == nullptr);
     List* l = m3->get_list_or_initialize("abc");
 
